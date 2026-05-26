654606d719d4672f08253648ef440e79daef8b0f
Espressohin <lamia524@outlook.fr>
2026-05-26 14:08:13 +0200

    "Overral home page design finished."

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+let gallerypics = [
+    //voir pour simplifier le html et faire array d'image ou json
+]
+
 document.getElementById('galleryNext').addEventListener('click', function() {
     const slider = document.querySelector('.gallery-track');
     slider.scrollBy({ left: 300, behavior: 'smooth' });
-    document.getElementById('galleryPrev').classList.remove('active');
-    document.getElementById('galleryNext').classList.add('active');
+    imageIndex++;
 });
+
+
+
+//les trucs à faire:
+// - faire le slider de la gallerie d'images carroussel
+// - faire le slider des events à venir
+// - toggle de la langue
+// - faire le changement d'onglet dans le sidebar
+// - faire le menu hamburger
+// - faire le formulaire de connexion
+// - faire le formulaire d'inscription
+// - faire le formulaire de contact
