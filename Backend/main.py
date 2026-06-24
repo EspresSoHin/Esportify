dcd9f43bd90eaf671289c2bd71ba892dd500f709
Espressohin <lamia524@outlook.fr>
2026-06-24 23:37:45 +0200

    Setup CORS et Auth

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from database import engine
-from routes import users , events, inscriptions, favoris, scores
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, HTTPBearer, HTTPAuthorizationCredentials
+from Oauth2 import create_access_token, decode_token
+from routes import users , events, inscriptions, favoris, scores, auth
+
 
 app = FastAPI()
+oauth2_scheme = HTTPBearer()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",   # local Live Server
+        "http://localhost:5500",    # d local alternative
+        "https://esportify-8t6x.onrender.com/"  # à mettre à jour avec le domaine
+    ],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allow_headers=["Content-Type", "Authorization"],
+)
+
 app.include_router(users.router)
 app.include_router(events.router)
 app.include_router(inscriptions.router)
 app.include_router(favoris.router)
 app.include_router(scores.router)
-
+app.include_router(auth.router)
